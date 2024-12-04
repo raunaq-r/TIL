bdc54bd4373928bb1112ab272f8aaae0af5aec79
Raunak R <raunaq.ranjan@outlook.com>
2024-12-05 00:42:09 +0530

    add vue project

---
+++ today-i-learnt/src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <h2>Welcome to TIL</h2>
+  </header>
+</template>
+
+<style scoped>
+</style>
---
+++ today-i-learnt/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h2>Welcome to TIL</h2>
+  </main>
+</template>
